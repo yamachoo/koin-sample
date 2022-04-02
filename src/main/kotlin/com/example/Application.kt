49cb084f76339779d65321a156aeadf5a9e69285
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-04-02 23:04:00 +0900

    add inject

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,11 +1,23 @@
 package com.example
 
+import com.example.module.helloAppModule
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import io.ktor.application.*
+import io.ktor.features.*
+import org.koin.ktor.ext.Koin
+import org.koin.logger.SLF4JLogger
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        install(DefaultHeaders)
+        install(CallLogging)
+        install(Koin) {
+            SLF4JLogger()
+            modules(helloAppModule)
+        }
+
         configureRouting()
     }.start(wait = true)
 }
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,12 +1,15 @@
 package com.example.plugins
 
+import com.example.service.HelloService
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
+import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
+    val service: HelloService by inject()
 
     // Starting point for a Ktor app:
     routing {
@@ -15,5 +18,8 @@ fun Application.configureRouting() {
         }
     }
     routing {
+        get("/hello") {
+            call.respondText(service.sayHello())
+        }
     }
 }
