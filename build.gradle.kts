4026574fce1b6cc68fa166f92863e5753267475e
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-04-02 23:39:07 +0900

    add koin annotations

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val koin_version: String by project
+val koin_ksp_version: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.6.10"
+    id("com.google.devtools.ksp") version "1.6.10-1.0.2"
 }
 
 group = "com.example"
@@ -21,12 +23,18 @@ repositories {
     mavenCentral()
 }
 
+sourceSets.main {
+    java.srcDirs("build/generated/ksp/main/kotlin")
+}
+
 dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.insert-koin:koin-ktor:$koin_version")
     implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+    implementation("io.insert-koin:koin-annotations:$koin_ksp_version")
+    ksp("io.insert-koin:koin-ksp-compiler:$koin_ksp_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
