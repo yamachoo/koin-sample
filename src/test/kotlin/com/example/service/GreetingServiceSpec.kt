8cd380e3fbf7ebce2087b9d4e93a1ded0b5cd369
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-04-03 16:52:29 +0900

    modify GreetingServiceSpec

--- src/test/kotlin/com/example/service/GreetingServiceSpec.kt
+++ src/test/kotlin/com/example/service/GreetingServiceSpec.kt
@@ -1,18 +1,37 @@
 package com.example.service
 
+import com.example.repository.GreetingRepository
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.koin.KoinExtension
 import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import org.koin.core.context.GlobalContext
+import org.koin.ksp.generated.defaultModule
 import org.koin.test.KoinTest
 import org.koin.test.inject
+import org.koin.test.mock.declareMock
 
 class GreetingServiceSpec : DescribeSpec(), KoinTest {
-    private val greetingService: GreetingService by inject()
-
     init {
+        val greetingService: GreetingService by inject()
+
+        extension(KoinExtension(defaultModule) {
+            mockk<GreetingRepository>()
+        })
+
         describe("use greetingService") {
             it("return valid value") {
-                greetingService.sayGreeting() shouldBe "Hello Ktor & Koin Annotations!"
+                declareMock<GreetingRepository> {
+                    every { getGreeting() } returns "Ktor & Koin Annotations by MockK"
+                }
+
+                greetingService.sayGreeting() shouldBe "Hello Ktor & Koin Annotations by MockK!"
             }
+        }
+
+        afterSpec {
+            GlobalContext.getOrNull() shouldBe null
         }
     }
 }
